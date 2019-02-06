7d194c09f0bfe3d7cf1a611faaf976db8450b230
Michael Krohn <mfkrohn@gmail.com>
2019-02-06 15:00:07 -0500

    Worked through the exercises and learned about fields constructors parameters methods and statements

--- TicketMachine.java
+++ TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kolling
  * @version 2008.03.30
  */
-public class TicketMachine
+  public class  TicketMachine
 {
     // The price of a ticket from this machine.
     private int price;
@@ -17,18 +17,31 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    //Exercise 2.15
+    private int status;
+    
+    
 
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int ticketCost)
+    public TicketMachine()
     {
-        price = ticketCost;
+        price = 1000;
         balance = 0;
         total = 0;
     }
+    
+    public TicketMachine(int inputPrice)
+    {
+     price = inputPrice;  
+    }
+    
+    
+        
+    
 
     /**
      * Return the price of a ticket.
@@ -52,9 +65,60 @@ public class TicketMachine
      */
     public void insertMoney(int amount)
     {
-        balance = balance + amount;
+        if(amount > 0) {
+         balance = balance + amount;   
+        }
+        else{
+         System.out.println("Use a positive amount: " + amount);   
+        }
     }
-
+    
+    public int getTotal()
+    {
+        return total;
+    }
+    
+    public void prompt()
+    {
+     System.out.println("Please insert the correct amount of money.");   
+    }
+    
+    public void showPrice()
+    {
+     System.out.println("The price of a ticket is " + price + " cents.");  
+    }
+    //Exercise 2.55
+    public int emptyMachine()
+    {
+        int totalRemoved = total;
+        total = 0;   
+        return totalRemoved;
+    }
+    //Exercise 2.56 emptyMAchine is both mutator and accessor becuase
+    //it returns a value to the caller while muating the total to zero 
+    public void setPrice(int newPrice)
+    {
+     price = newPrice;   
+    }
+    //Exercise 2.49
+    public void currentDiscount(int discount)
+    {
+        int saving = price * discount;
+    }
+    //Exercise 2.50
+    // int mean = total / count
+    
+    /**Exercise 2.51 and 2.52
+     * 
+     * if(price > budget){
+     * System.out.println("Too Expensive! Your budget is currently: "
+     * + budget);
+     * }
+     * else{
+     * System.out.println("Just Right");
+     * }
+     */
+    //Execise 2.53 return should be the final line of the method
     /**
      * Print a ticket.
      * Update the total collected and
@@ -62,6 +126,10 @@ public class TicketMachine
      */
     public void printTicket()
     {
+        int amountLeftToPay = price - balance;
+        if(amountLeftToPay <= 0){
+            
+      
         // Simulate the printing of a ticket.
         System.out.println("##################");
         System.out.println("# The BlueJ Line");
@@ -71,8 +139,34 @@ public class TicketMachine
         System.out.println();
 
         // Update the total collected with the balance.
-        total = total + balance;
-        // Clear the balance.
-        balance = 0;
+        total += price;
+        //Reduce balance by price to get change
+        balance -= price;
+    }
+    else{
+        System.out.println("You must insert at least: " 
+        + amountLeftToPay + " more cents");
+        
+        
     }
 }
+    public int refundBalance()
+    {
+      int amountToRefund;
+      amountToRefund = balance;
+      balance = 0;
+      return amountToRefund;
+}
+}
+/** Exercise 2.58
+ * In order to change ticket price you could set up the constructor to ask 
+ * what type of ticket you want when the ticket machine is created. The amount
+ * new fields created depends on the amount of ticket choices there are. Each
+ * one of these fields would set a different price for the ticket. The rest of
+ * code would behave the same once the ticket price was set.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
